f9332b54787de260b050766d72b0f45c520b5858
Scooter Wadsworth <scooter.wadsworth@hey.com>
2025-02-01 21:17:06 -0500

    Update linux-install.sh script to enable kanata.service

--- kanata/linux-install.sh
+++ kanata/linux-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 install ./bin/kanata /usr/local/bin/kanata
-cp ./kanata.service /lib/systemd/system/kanata.service
-cp ./devone.kdb /etc/kanata/kanata.kbd
 mkdir -p /etc/kanata
+cp ./devone.kdb /etc/kanata/kanata.kbd
+cp ./kanata.service /lib/systemd/system/kanata.service
+systemctl start kanata
+systemctl enable kanata
